8ae63f133c273199a425849c2ac9efc1914a4ecc
jaiyankargupta <jaiyankargupta@gmail.com>
2025-05-06 00:49:34 +0530

    add web logo

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -39,7 +39,7 @@ export default function Contact() {
                 <Grid xs={12} md={6}>
                   <TextField label="Your Role*" fullWidth variant="outlined" margin="dense" />
                 </Grid>
-                <Grid xs={12}>
+                <Grid xs={4}>
                   <TextField 
                     label="Message*" 
                     fullWidth 
